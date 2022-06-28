1b35a666af05c8992cfb77eab012d0b87f03afe1
eziel <eziielp10@hotmail.com>
2022-06-28 19:50:38 -0300

    end Responsive

--- src/components/pages/crew/styled.ts
+++ src/components/pages/crew/styled.ts
@@ -8,13 +8,13 @@ function pixelsToRem(...values:Array<number>) {
 const MainCrew = styled.div`
   width: 1500px;
   height: 80%;
-  margin: ${pixelsToRem(50)} auto 0 auto;
   display: flex;
   flex: 1;
   flex-direction: column;
   opacity: 0;
   transform: translateX(-40px);
   animation: .5s ToRight forwards;
+  padding: 1rem;
   
   @keyframes ToRight {
     to {
@@ -22,12 +22,43 @@ const MainCrew = styled.div`
       opacity: initial;
     }
   }
+  @media (max-width: 1540px) {
+    margin-top: 3rem;
+    width: 1000px;
+  }
+ 
+  @media (max-width: 1030px) {
+    width: 800px;
+  }
+ 
+  @media (max-width: 900px) {
+    width: 700px;
+  }
+  @media (max-width: 720px) {
+    height: 90%;
+    width: 600px;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr;
+    justify-content: start;
+    align-items: start;
+    flex: 0;
+  }
+  @media (max-width: 600px) {
+    width: 500px;
+  }
+  @media (max-width: 500px) {
+    width: 350px;
+  }
 `
 const TitlePage = styled.span`
   font: var(--font-default);
   letter-spacing: 3px;
   text-transform: uppercase;
   color: var(--light2);
+  @media (max-width: 500px) {
+    max-height: 25px;
+  }
 `
 
 
--- src/components/pages/technology/styled.ts
+++ src/components/pages/technology/styled.ts
@@ -7,7 +7,6 @@ function pixelsToRem(...values:Array<number>) {
 
 const MainTech = styled.div`
   width: 1500px;
-  margin: ${pixelsToRem(50)} auto 0 auto;
   display: flex;
   flex-direction: column;
   opacity: 0;
@@ -22,6 +21,34 @@ const MainTech = styled.div`
       opacity: initial;
     }
   }
+  @media (max-width: 1540px) {
+      margin-top: 4rem;
+      width: 1200px;
+  }
+  @media (max-width: 1250px) {
+      width: 1000px;
+  }
+  @media (max-width: 1035px) {
+      width: 900px;
+  }
+  @media (max-width: 950px) {
+      width: 800px;
+  }
+  @media (max-width: 850px) {
+      width: 700px;
+  }
+  @media (max-width: 750px) {
+      width: 600px;
+  }
+  @media (max-width: 650px) {
+      width: 500px;
+  }
+  @media (max-width: 550px) {
+      width: 400px;
+  }
+  @media (max-width: 450px) {
+      width: 300px;
+  }
 `
 const Title = styled.h1`
   font: var(--font-default);
@@ -34,6 +61,11 @@ const Container = styled.main`
   margin: 5rem 0;
   gap: 2rem;
   height: 100%;
+
+  @media (max-width: 950px) {
+    display: grid;
+    grid-template-columns: 1fr  ;
+  }
 `
 
 
